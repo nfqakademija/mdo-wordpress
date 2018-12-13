40412777b939661de6c77951660d80d2873008ad
Mantas Marcinkevicius <mantas.marcinkevicius@nfq.lt>
2018-12-13 22:24:16 +0200

    added deployment directory

---
+++ .deploy/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# This script prepares all dependent files to be deployed to server as a single .tar file
+# This script intended to be executed in Jenkins CI, but likelly to be extended by you.
+
+set -e # Stop on error
+set -x # Show commands being executed
+
